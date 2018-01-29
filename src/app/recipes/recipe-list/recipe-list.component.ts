b749b301588f16f3c60627477ab7f518075db3f8
Mariusz Maciejczuk <mariusz.maciejczuk@intive.com>
2018-01-29 09:01:23 +0100

    changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is test', 
+    'https://i.vimeocdn.com/portrait/58832_300x300'),
+    new Recipe('A Test Recipe', 'This is test', 
+    'https://i.vimeocdn.com/portrait/58832_300x300')
+  ];
 
   constructor() { }
 
